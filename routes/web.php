a4017c3bc1b966ebd14b218f7a78d8cc3143d6e9
Thien Ngoc <ngocnt@funtap.vn>
2020-08-12 14:11:23 +0700

    u

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::phase('/', 'BlogController@HomePage');
-Route::phase('/about', 'BlogController@AboutPage');
-Route::phase('/contact', 'BlogController@ContactPage');
-Route::phase('/posts/{article}', 'BlogController@SingleArticle');
+Route::post('/webhook', function ($req, $res) {
+    $access_token = "EAAH5qCyWCdcBAP0ddTZBNbVRdmqd43TZCnBJGFEwRZAmO76hlrXfWmVzBXO5xEsochEnlrQ88Tkrwm2B63KzXctLxXQ8RU6KKM9sWEFsGZAaBzmmMUoqVjfir1n5ufXgW8btvZAL41bNJ5S0IceHKUCioOLTqCLZCZCOOMlNz5fRAZDZD";
+    $verify_token = "ma_xac_minh_cua_ban";
+    $hub_verify_token = 'error';
+    if (isset($req["hub_challenge"])) {
+        $challenge = $req["hub_challenge"];
+        $hub_verify_token = $req["hub_verify_token"];
+    }
+    if ($hub_verify_token === $verify_token) {
+        echo $challenge;
+    }
+});
